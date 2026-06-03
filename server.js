bb2a6e4e610a7d17330701b53015253216f1e7fe
Vinod V <vinod@tunerlabs.com>
2026-06-03 14:31:27 +0530

    Deployment related file changes done for saathi

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 1819;
+const port = process.env.PORT || 1829;
 
 app.use('/', express.static(path.join(__dirname, 'build')));
 
